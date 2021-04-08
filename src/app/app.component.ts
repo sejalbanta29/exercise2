0f4526a8210d5bafc93968f8c1d6e995062efbff
sejalbanta29 <soodsejal5@gmail.com>
2021-04-08 23:30:23 +0530

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ValidatePassword } from './validate-password';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercise2';
-}
+  title = 'first-app';
+  submitted = false;
+  
+  constructor(public fb: FormBuilder) {}
+
+  registrationForm = this.fb.group({
+    fullName: this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[_A-z0-9]*((-|\s)*[_A-z0-9])*$')]],
+      lastName: ['', [Validators.required]]
+    }),
+    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    PasswordValidation: this.fb.group({
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    },{
+        validator: ValidatePassword.MatchPassword
+      })
+    })
+
+    //getter method to access formcontrols
+    get myForm() {
+      return this.registrationForm.controls;
+    }
+
+    //submit form
+    onSubmit() {
+      this.submitted = true;
+      if(!this.registrationForm.valid){
+        alert('Please fill all required fields')
+        return false;
+      } else {
+        console.log(this.registrationForm.value);
+      }
+    }
+
+} 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthGuard } from './auth-guard.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ResolveService } from './resolver';
+
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducer/demo.reducer';
+import {ReadComponent } from './read/read.component';
+import {CreateComponent } from './create/create.component';
+
+
+const appRoutes : Routes = [
+  {
+    path: 'dashboard', component:DashboardComponent, canActivate:[AuthGuard],resolve:{news:ResolveService}
+  },
+  {
+    path: 'login',component:AppComponent
+  },
+  {
+    path: '404',component: NotfoundComponent
+  }
+  
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotfoundComponent,
+    ReadComponent,
+    CreateComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      tutorial: reducer
+    }),
+    ReactiveFormsModule
+    
   ],
-  providers: [],
+  providers: [ResolveService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
